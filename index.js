30df2287816729f0408e2e26b41e75841a996c48
Alan Dega <arkidega@gmail.com>
2020-01-15 17:39:57 -0600

    c5

--- index.js
+++ index.js
@@ -17,4 +17,4 @@ app.use(
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => console.log(`Servidor corriendo en el puerto${PORT}`));
+app.listen(port, () => console.log(`Servidor corriendo en el puerto${port}`));
